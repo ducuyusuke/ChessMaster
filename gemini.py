ff29392a945e0572c0b169057017ef53797adf87
Eduardo <ducu_yusuke@hotmail.com>
2024-08-31 15:57:54 -0300

    altered from suggestions to evaluations

--- gemini.py
+++ gemini.py
@@ -1,5 +1,6 @@
 import os
 import google.generativeai as genai
+from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
@@ -7,8 +8,8 @@ load_dotenv()
 # Access the API key
 genai.configure(api_key=os.getenv("API_KEY"))
 
-def coach_answer(fen):
+def coach_answer(fen, move):
     gemini = genai.GenerativeModel("gemini-pro")
-    response = gemini.generate_content(f"In a Classic Chess game, you must analyse the state of the board according to FEN notation: {fen} . Suggest me 3 legal moves (pay close attetion to the current position of the pieces) and very briefly explain why they are good moves, as if you were a coach. Send me the response in this format: '1: move_notation -> brief_explanation. 2: move_notation -> brief_explanation. 3: move_notation -> brief_explanation.'.")
+    response = gemini.generate_content(f"In a Classic Chess game, you must analyse the state of the board according to FEN notation: {fen} and then analyse White's latest move in that context: {move}. Given the state of the board and the latest move played, I want you to briefly explain whether it was a good move or not and why, as if you were a coach. Send me the response in a single paragraph and don't include any greetings, only the overall anaylsis.")
     print(response.text)
     return response.text
